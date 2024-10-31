3d90107ec51044d10f2550f28e196ad10e5c8f7e
Omar Abdelrahim <o.abdelrahim@artcenter.local>
2024-10-31 16:20:22 +0300

    new upload for the pproject

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let mainProduct = document.getElementById("main-product");
+let message = document.getElementById("message");
+let love = document.getElementById("love");
 let container = document.querySelector(".container");
 
 let changeProduct = (product) => {
@@ -8,3 +10,15 @@ let changeProduct = (product) => {
 let changeBg = (color) => {
   container.style.background = color;
 };
+
+let loveMsg = () => {
+  message.style.left = "50%";
+  message.style.top = "40%";
+};
+let loveMsgLeave = () => {
+  message.style.left = "1000px";
+  message.style.top = "1000px";
+};
+
+love.addEventListener("click", loveMsg);
+love.addEventListener("dblclick", loveMsgLeave);
